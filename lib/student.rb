200a394a05ef6d42f2a6051594bb4a1c96215a1a
Charles Pelton <charliejp0311@gmail.com>
2020-02-13 00:00:26 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,23 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+  attr_accessor :knowledge
+  @@all = []
+
+  def initialize
+      @knowledge = []
+      save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def learn(information)
+    @knowledge << information
+  end
 
 end
