cc2d8d946febc0f672326d6002f4228fa1660038
Xavier Perez <xperez007@gmail.com>
2020-02-22 16:36:40 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require 'pry'
 
+class Student 
+  attr_accessor :first_name, :last_name
+   
+   @@all = all
+  
+  def initialize(knowledge, learn)
+    @knowledge = knowledge
+    @learn = learn 
+    @@all = self
+    
+  end
 end
